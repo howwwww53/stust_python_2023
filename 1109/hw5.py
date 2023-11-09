800f8db7f4427c3e56860e23ebe2bb188ddca44a
howwwww53 <4b0g0134@stust.edu.tw>
2023-11-09 13:09:43 +0800

    Update hw5.py

--- 1109/hw5.py
+++ 1109/hw5.py
@@ -1,7 +1,11 @@
 def outer(a,b):
+    a**2
     def add(a,b):
         sum=a+b
         return sum
-        result= add(a,b)    
+    result= add(a,b)  
     result=result+5
     return result
+
+result = outer(5, 10)
+print(result)
